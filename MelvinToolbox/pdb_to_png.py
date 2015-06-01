92a457526a85f425c80cd429b57d7ea2dc2ab328
Ryan Melvin <melvrl13@wfu.edu>
2015-06-01 12:39:51 -0400

    Python script in MelvinToolbox for generating a png from a pdb without user interaction. Requires pymol installation and the pymol scripts folder to be in the python search path.

---
+++ MelvinToolbox/pdb_to_png.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#Modified from https://www.biostars.org/p/42474/
+import __main__
+__main__.pymol_argv = [ 'pymol', '-qc'] # Quiet and no GUI
+
+import sys, time, os
+import pymol
+
+pymol.finish_launching()
+
+##
+# Read User Input
+spath = os.path.abspath(sys.argv[1])
+sname = spath.split('/')[-1].split('.')[0]
+
+# Load Structures
+
+pymol.cmd.load(spath, sname)
+pymol.cmd.disable("all")
+pymol.cmd.enable(sname)
+
+#Prepare name for image file
+outname = os.path.dirname(spath) + '/' + os.path.splitext(sname)[0] + '.png'
+pymol.cmd.png(outname)
+
+# Get out!
+pymol.cmd.quit()
