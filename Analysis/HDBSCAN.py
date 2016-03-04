57bfe216f940c7231d833c295c80c1ec45685aa9
Ryan Melvin <melvrl13@wfu.edu>
2016-03-04 14:39:44 -0500

    A clustering algorithm HDBSCAN that I think could be immediately useful. I'll show it off during group meeting.

---
+++ Analysis/HDBSCAN.py
@@ -0,0 +1,63 @@
+import numpy as np
+import MDAnalysis as md
+import hdbscan
+from sklearn.metrics import silhouette_samples, silhouette_score
+import matplotlib
+matplotlib.use('Agg') #For use on DEAC cluster
+import matplotlib.pyplot as plt
+plt.style.use('bmh')
+import argparse
+
+# Currently only working in python 2 due to MDAnalysis package.
+# Initialize parser. The default help has poor labeling. See http://bugs.python.org/issue9694
+parser = argparse.ArgumentParser(description = 'Run and score hdbscan clustering', add_help=False) 
+
+# List all possible user input
+inputs=parser.add_argument_group('Input arguments')
+inputs.add_argument('-h', '--help', action='help')
+inputs.add_argument('-top', action='store', dest='structure',help='Structure file corresponding to trajectory',type=str,required=True)
+inputs.add_argument('-traj', action='store', dest='trajectory',help='Trajectory',type=str,required=True)
+inputs.add_argument('-sel', action='store', dest='sel', help='Atom selection',type=str,default='not name H*')
+inputs.add_argument('-min', action='store', dest='min', help='minimum cluster membership',type=int,default=10)
+inputs.add_argument('-o', action='store', dest='out_name',help='Output file',type=str,required=True)
+
+# Parse into useful form
+UserInput=parser.parse_args()
+
+topology = UserInput.structure
+trajectory = UserInput.trajectory
+t = md.Universe(topology,trajectory)
+sel = t.select_atoms(UserInput.sel)
+
+# Format trajectory 
+temp = t.trajectory.timeseries(sel,format=u'fac')
+frames = len(t.trajectory)
+atoms = len(sel.atoms)
+t = []
+data = temp.reshape((frames,atoms*3))
+temp = []
+
+# Run hdbscan
+clusterer = hdbscan.HDBSCAN(min_cluster_size=UserInput.min)
+cluster_labels = clusterer.fit_predict(data)
+raw_score = silhouette_score(data,cluster_labels)
+
+# Calculate silhouette score ignoring points HDBSCAN treated as outliers
+scoreable = np.where(cluster_labels>-1)[0]
+scores = silhouette_samples(data,cluster_labels)
+adjusted_score = np.mean(scores[scoreable])
+
+# Save results
+#Text
+np.savetxt(UserInput.out_name + '/hdbscan_labels.txt', cluster_labels, fmt='%i')
+with open (UserInput.out_name + '/silhouette_scores.txt', 'w') as f:
+    f.write("silhouette score is {0} \n adjusted score is {1}".format(raw_score, adjusted_score))
+    
+#Figures
+plt.figure()
+plt.scatter(np.arange(frames), cluster_labels, marker = '+')
+plt.xlabel('Frame')
+plt.ylabel('Cluster')
+plt.title('HDBSCAN')
+plt.savefig(UserInput.out_name + '/hdbscan_timeseries.png')
+plt.clf()
