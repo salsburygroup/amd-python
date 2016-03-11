e9eeafd87b210e892c3d719de79276b944cb5197
Ryan Melvin <melvrl13@wfu.edu>
2016-03-11 12:46:11 -0500

    Added a feature to HDBSCAN to predict the cutoff for QT clustering in VMD. Also, HDBSCAN is now python3 compatible.

--- Analysis/HDBSCAN.py
+++ Analysis/HDBSCAN.py
@@ -1,5 +1,5 @@
 import numpy as np
-import MDAnalysis as md
+import mdtraj as md
 import hdbscan
 from sklearn.metrics import silhouette_samples, silhouette_score
 import matplotlib
@@ -17,7 +17,7 @@ inputs=parser.add_argument_group('Input arguments')
 inputs.add_argument('-h', '--help', action='help')
 inputs.add_argument('-top', action='store', dest='structure',help='Structure file corresponding to trajectory',type=str,required=True)
 inputs.add_argument('-traj', action='store', dest='trajectory',help='Trajectory',type=str,required=True)
-inputs.add_argument('-sel', action='store', dest='sel', help='Atom selection',type=str,default='not name H*')
+inputs.add_argument('-sel', action='store', dest='sel', help='Atom selection',type=str,default='not element H')
 inputs.add_argument('-min', action='store', dest='min', help='minimum cluster membership',type=int,default=10)
 inputs.add_argument('-o', action='store', dest='out_name',help='Output file',type=str,required=True)
 
@@ -26,21 +26,34 @@ UserInput=parser.parse_args()
 
 topology = UserInput.structure
 trajectory = UserInput.trajectory
-t = md.Universe(topology,trajectory)
-sel = t.select_atoms(UserInput.sel)
+t = md.load(trajectory,top=topology)
+sel = t.topology.select(UserInput.sel)
+t = t.atom_slice(sel)
 
 # Format trajectory 
-temp = t.trajectory.timeseries(sel,format=u'fac')
-frames = len(t.trajectory)
-atoms = len(sel.atoms)
-t = []
+temp = t.xyz
+frames = t.xyz.shape[0]
+atoms = t.xyz.shape[1]
 data = temp.reshape((frames,atoms*3))
+data = data.astype('float64')
 temp = []
 
 # Run hdbscan
 clusterer = hdbscan.HDBSCAN(min_cluster_size=UserInput.min)
 cluster_labels = clusterer.fit_predict(data)
 raw_score = silhouette_score(data,cluster_labels)
+
+# Predict QT cutoff
+labels_reduced = cluster_labels[cluster_labels != -1]
+counts = np.bincount(labels_reduced)
+biggest_cluster = np.argmax(counts)
+frames_big_cluster = np.where(cluster_labels == biggest_cluster)
+biggest_cluster_traj = t.slice(frames_big_cluster)
+# Make dissimilarity matrix for biggest cluster
+distances = np.empty((biggest_cluster_traj.n_frames, biggest_cluster_traj.n_frames))
+for i in range(biggest_cluster_traj.n_frames):
+    distances[i] = md.rmsd(biggest_cluster_traj, biggest_cluster_traj, i)
+print('Predicted QT diameter: %f nm' % np.max(distances))
 
 # Calculate silhouette score ignoring points HDBSCAN treated as outliers
 scoreable = np.where(cluster_labels>-1)[0]
