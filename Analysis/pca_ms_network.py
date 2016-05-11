c19cac09db953bc01af393826283a04bb04bbd07
Ryan Godwin <godwrc12@wfu.edu>
2016-05-11 12:28:15 -0400

    updating pca_ms_network.py with additional options

--- Analysis/pca_ms_network.py
+++ Analysis/pca_ms_network.py
@@ -7,6 +7,7 @@ import matplotlib
 matplotlib.use('Agg') #Avoids hangs on visualize
 import matplotlib.pyplot as plt
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description = 'Turn PCA space into a network based on wells', add_help=False) 
 
@@ -15,7 +16,7 @@ inputs=parser.add_argument_group('Input arguments')
 inputs.add_argument('-h', '--help', action='help')
 inputs.add_argument('-top', action='store', dest='structure',help='Structure file corresponding to trajectory',type=str,required=True)
 inputs.add_argument('-traj', action='store', dest='trajectory',help='Trajectory',type=str,required=True)
-inputs.add_argument('-sel', action='store', dest='sel', help='Atom selection',type=str,default='not element H')
+inputs.add_argument('-sel', action='store', dest='sel', help='Atom selection',type=str,default='name CA')
 inputs.add_argument('-o', action='store', dest='out_name',help='Output file',type=str,required=True)
 
 # Parse into useful form
@@ -38,7 +39,9 @@ temp = []
 # Make PCA projection
 projection = PCA().fit_transform(data)
 
-num_components = projection.shape[1]
+
+#num_components = projection.shape[1]
+num_components=5
 network = nx.Graph()
 nodes = np.arange(0,num_components)
 network.add_nodes_from(nodes)
@@ -47,10 +50,14 @@ label_dict = dict(zip(nodes,nodes))
 for i in range(0,num_components-1):
     for j in range(i+1, num_components):
         temp = np.column_stack((projection[:,i], projection[:,j]))
-        labels = MeanShift(n_jobs=-1).fit_predict(temp)
+        labels = MeanShift(n_jobs=-1,cluster_all=True).fit_predict(temp)
         num_clusters = max(labels) + 1 #0-based index
+        print(np.unique(labels))
+        progress = "\r Calculation on Component " + str(i) + " of " + str(num_components-1) #status 
+        sys.stdout.write(progress)
+        sys.stdout.flush() #report status to terminal output
         if num_clusters > 1:
             network.add_edge(i,j)
 
 nx.draw_spring(network, labels=label_dict)
-plt.savefig(UserInput.out_name + '/PCA_network.png')
+plt.savefig(UserInput.out_name + '/PCA_network.100.png')
