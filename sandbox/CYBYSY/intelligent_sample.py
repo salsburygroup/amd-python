5e7c1bbea4dd594ae05e33db382bec2e762af985
Ryan Melvin <melvrl13@wfu.edu>
2016-10-07 11:51:09 -0400

    Process for docking screening with Vina in the new 'Examples' directory

--- sandbox/CYBYSY/intelligent_sample.py
+++ sandbox/CYBYSY/intelligent_sample.py
@@ -47,59 +47,68 @@ with open(UserInput.configuration_file, "r") as input_conf:
     lines = input_conf.readlines()
 with open(UserInput.configuration_file, "r") as input_conf:
     configuration_text = input_conf.read()
+    
+desired_steps = int(re.findall(r'run\s+(\d+)', configuration_text)[0])
+topology = re.findall(r'structure\s+(.*)', configuration_text)[0]
+pdb_file = re.findall(r'coordinates\s+(.*)', configuration_text)[0]
+trajectory = re.findall(r'dcdfile\s+(.*)', configuration_text)[0]
+sample_rate = int(re.findall(r'dcdfreq\s+(\d+)', configuration_text)[0])
+desired_frames = int(math.ceil(desired_steps / sample_rate))
+del configuration_text
+
 with open(UserInput.configuration_file, "w") as input_conf:
     for line in lines:
         input_conf.write(re.sub(r'dcdfreq\s+\d+', r'dcdfreq\t1', line))
 del lines
 
-desired_steps = int(re.findall(r'run\s+(\d+)', configuration_text)[0])
-topology = re.findall(r'structure\s+(.*)', configuration_text)[0]
-trajectory = re.findall(r'dcdfile\s+(.*)', configuration_text)[0]
-tenth_nanoseconds = int(math.ceil(desired_steps / 25000))
-del configuration_text
-
 with open(UserInput.configuration_file, "r") as input_conf:
     lines = input_conf.readlines()
 
-for p1_ns in range(tenth_nanoseconds):
-    os.mkdir(str(p1_ns))
-    with open('input', "w+") as short_config:
-        for line in lines:
-            short_config.write(re.sub(r'run\s+\d+', r'run\t' + str(25000 * (p1_ns + 1)), line))
-    acemd_cmd = 'acemd --device ' + UserInput.device + ' input>&log_' + str(p1_ns)
-    process = subprocess.Popen(acemd_cmd, shell=True)
-    process.wait()
-    t = mdtraj.load(trajectory, top=topology)
-    sel = t.topology.select(UserInput.sel)
-    t_slice = t.atom_slice(sel)
-    t_slice = t_slice.superpose(t_slice)
-    t_slice = t_slice[p1_ns:25000 + p1_ns - 1]
-    temp = t_slice.xyz
-    frames = t_slice.xyz.shape[0]
-    atoms = t_slice.xyz.shape[1]
-    data = temp.reshape((frames, atoms * 3))
-    data = data.astype('float64')
-    del temp
-    clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
-    cluster_labels = clusterer.fit_predict(data)
-    num_frames = len(cluster_labels)
-    labeled_traj = pandas.DataFrame(columns=['frame', 'cluster'])
-    labeled_traj['frame'] = numpy.arange(num_frames)
-    labeled_traj['cluster'] = cluster_labels
-    for i in range(0, int(max(cluster_labels)) + 1):
-        cluster_string = labeled_traj.loc[labeled_traj['cluster'] == i].frame.values
-        cluster_coords = data[cluster_string]
-        mean = cluster_coords.mean(axis=0)
-        distance = [euclidean(row, mean) for row in cluster_coords]
-        rep = cluster_string[numpy.argmin(distance)]
-        t_slice[rep].save_pdb(str(p1_ns) + '/rep' + str(i) + '.pdb')
-    plt.figure()
-    plt.scatter(numpy.arange(frames), cluster_labels, marker='+')
-    plt.xlabel('Frame')
-    plt.ylabel('Cluster')
-    plt.title('HDBSCAN')
-    plt.savefig(str(p1_ns) + '/hdbscan_timeseries.png')
-    plt.close()
-    del t_slice
-    t[-1:-1 * (p1_ns + 1)].save_dcd(trajectory)
-    del t
+psf = mdtraj.load_topology(topology)
+pdb = mdtraj.load(pdb_file)
+atoms_of_interest = psf.select(UserInput.sel)
+n_atoms_of_interest = len(atoms_of_interest)
+reference_structure = pdb.atom_slice(atoms_of_interest)
+not_water = psf.select("not water")
+
+with mdtraj.formats.DCDTrajectoryFile('temp.dcd', 'w') as strided_trajectory:
+    for output_frame in range(desired_frames):
+        xyz = numpy.zeros((sample_rate, n_atoms_of_interest, 3), dtype='float64')
+        if not os.path.exists(str(output_frame)):
+            os.mkdir(str(output_frame))
+        with open('input', "w+") as short_config:
+            for line in lines:
+                short_config.write(re.sub(r'run\s+\d+', r'run\t' + str(sample_rate * (output_frame + 1)), line))
+        acemd_cmd = 'acemd --device ' + UserInput.device + ' input>&log_' + str(output_frame)
+        process = subprocess.Popen(acemd_cmd, shell=True)
+        process.wait()
+        for frame in range(output_frame, output_frame + sample_rate):
+            current_frame = mdtraj.load_frame(trajectory, frame, top=topology, atom_indices=atoms_of_interest)
+            current_frame = current_frame.superpose(reference_structure)
+            xyz[frame - output_frame, :, :] = current_frame.xyz
+        xyz = xyz.astype('float64')
+        clusterer = hdbscan.HDBSCAN(min_cluster_size=10)
+        cluster_labels = clusterer.fit_predict(xyz)
+        labeled_traj = pandas.DataFrame(columns=['frame', 'cluster'])
+        labeled_traj['frame'] = numpy.arange(sample_rate)
+        labeled_traj['cluster'] = cluster_labels
+        for i in range(0, int(max(cluster_labels)) + 1):
+            cluster_string = labeled_traj.loc[labeled_traj['cluster'] == i].frame.values
+            cluster_coords = xyz[cluster_string]
+            mean = cluster_coords.mean(axis=0)
+            distance = [euclidean(row, mean) for row in cluster_coords]
+            rep = cluster_string[numpy.argmin(distance)]
+            mdtraj.load_frame(trajectory, rep, top=topology, atom_indices=not_water
+                              )[rep].save_pdb(str(output_frame) + '/rep' + str(i) + '.pdb')
+        del xyz
+        plt.figure()
+        plt.scatter(numpy.arange(sample_rate), cluster_labels, marker='+')
+        plt.xlabel('Frame')
+        plt.ylabel('Cluster')
+        plt.title('HDBSCAN')
+        plt.savefig(str(output_frame) + '/hdbscan_timeseries.png')
+        plt.close()
+        strided_trajectory.write(mdtraj.load_frame(trajectory, -1, top=topology).xyz)
+        mdtraj.load_frame(trajectory, -1, top=topology).save_dcd(trajectory)
+os.remove(trajectory)
+os.rename('temp.dcd', trajectory)
