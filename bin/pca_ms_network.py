029d55c0b0fae804e8aafacb64f2265c728b3249
Ryan Melvin <melvrl13@wfu.edu>
2017-05-29 13:22:07 -0400

    pca_network_ms.py now saves weighted adjacency network in addition to network image.

--- bin/pca_ms_network.py
+++ bin/pca_ms_network.py
@@ -4,60 +4,61 @@ from sklearn.decomposition import PCA
 from sklearn.cluster import MeanShift
 import networkx as nx
 import matplotlib
-matplotlib.use('Agg') #Avoids hangs on visualize
+matplotlib.use('Agg')  # Avoids hangs on visualize
 import matplotlib.pyplot as plt
 import argparse
 import sys
 
-parser = argparse.ArgumentParser(description = 'Turn PCA space into a network based on wells', add_help=False) 
+parser = argparse.ArgumentParser(description='Turn PCA space into a network based on wells', add_help=False)
 
-# List all possible user input
-inputs=parser.add_argument_group('Input arguments')
+#  List all possible user input
+inputs = parser.add_argument_group('Input arguments')
 inputs.add_argument('-h', '--help', action='help')
-inputs.add_argument('-top', action='store', dest='structure',help='Structure file corresponding to trajectory',type=str,required=True)
-inputs.add_argument('-traj', action='store', dest='trajectory',help='Trajectory',type=str,required=True)
-inputs.add_argument('-sel', action='store', dest='sel', help='Atom selection',type=str,default='name CA')
-inputs.add_argument('-o', action='store', dest='out_name',help='Output file',type=str,required=True)
+inputs.add_argument('-top', action='store', dest='structure', help='Structure file corresponding to trajectory',
+                    type=str, required=True)
+inputs.add_argument('-traj', action='store', dest='trajectory', help='Trajectory', type=str, required=True)
+inputs.add_argument('-sel', action='store', dest='sel', help='Atom selection', type=str, default='name CA')
+inputs.add_argument('-o', action='store', dest='out_name', help='Output prefix', type=str, required=True)
 
-# Parse into useful form
-UserInput=parser.parse_args()
+#  Parse into useful form
+UserInput = parser.parse_args()
 
 topology = UserInput.structure
 trajectory = UserInput.trajectory
-t = md.load(trajectory,top=topology)
+t = md.load(trajectory, top=topology)
 sel = t.topology.select(UserInput.sel)
 t = t.atom_slice(sel)
 
-# Format trajectory 
+#  Format trajectory
 temp = t.xyz
 frames = t.xyz.shape[0]
 atoms = t.xyz.shape[1]
-data = temp.reshape((frames,atoms*3))
+data = temp.reshape((frames, atoms*3))
 data = data.astype('float64')
 temp = []
 
-# Make PCA projection
-projection = PCA().fit_transform(data)
+#  Make PCA projection
+projection = PCA(n_components=0.95, svd_solver="full").fit_transform(data)
 
 
-#num_components = projection.shape[1]
-num_components=5
+num_components = projection.shape[1]
 network = nx.Graph()
-nodes = np.arange(0,num_components)
+nodes = np.arange(0, num_components)
 network.add_nodes_from(nodes)
-label_dict = dict(zip(nodes,nodes))
+label_dict = dict(zip(nodes, nodes))
 
-for i in range(0,num_components-1):
+for i in range(0, num_components-1):
+    progress = "\r Calculation on Component " + str(i) + " of " + str(num_components - 1)  # status
+    sys.stdout.write(progress)
+    sys.stdout.flush()  # report status to terminal output
     for j in range(i+1, num_components):
-        temp = np.column_stack((projection[:,i], projection[:,j]))
-        labels = MeanShift(n_jobs=-1,cluster_all=True).fit_predict(temp)
-        num_clusters = max(labels) + 1 #0-based index
-        print(np.unique(labels))
-        progress = "\r Calculation on Component " + str(i) + " of " + str(num_components-1) #status 
-        sys.stdout.write(progress)
-        sys.stdout.flush() #report status to terminal output
+        temp = np.column_stack((projection[:, i], projection[:, j]))
+        labels = MeanShift(n_jobs=-1, cluster_all=True).fit_predict(temp)
+        num_clusters = max(labels) + 1  # 0-based index
         if num_clusters > 1:
-            network.add_edge(i,j)
+            network.add_edge(i, j, weight=num_clusters)
 
 nx.draw_spring(network, labels=label_dict)
-plt.savefig(UserInput.out_name + '/PCA_network.100.png')
+adjacency_matrix = nx.convert_matrix.to_numpy_matrix(network)
+np.savetxt(UserInput.out_name + '_PCA_adjacency_95.txt', adjacency_matrix, fmt='%1.0f')
+plt.savefig(UserInput.out_name + '_PCA_network_95.png')
